67158c5117d2bf2b0eb3e6f8e262d8ffd7dbbe52
shinkenjoe <simomayer@gmail.com>
2014-05-27 00:29:07 +0200

    complete crud chapter end version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,26 @@ class MoviesController < ApplicationController
     id = params[:id]
     @movie = Movie.find(id)
   end
+  def new
+  end
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes! (params[:movie])
+    flash[:notice]= "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice]= "#{@movie.title} was successfully deleted."
+    
+
 end
